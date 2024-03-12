16bfb3d72a3d726e069d704aaaba42be05e50a79
phuct-storai <phuc.t@stor.ai>
2024-03-12 23:18:40 +0700

    Commit

--- Services/Interfaces/IBookingTableService.cs
+++ Services/Interfaces/IBookingTableService.cs
@@ -4,10 +4,11 @@ namespace POSWindowFormAPI.Services.Interfaces
 {
     public interface IBookingTableService
     {
-        public string GetAllBookings();
+        public Task<string> GetAllBookings();
         public string CreateBooking(BookingTableDetail bookingTableDetail);
         public void UpdateBooking(string name, BookingTableDetail bookingTableDetail);
         public void DeleteBooking(string name);
         public bool GetByName(string name);
+        public string AddAnniversaryType(AnniversaryType anniversaryType);
     }
 }
